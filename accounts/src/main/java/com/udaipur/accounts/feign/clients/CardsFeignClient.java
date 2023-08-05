e1704d9651794cc60e443c3c5d37fd07ffbab12f
Sidhraj21 <sidhraj.solanki@gmail.com>
2023-08-05 22:29:18 +0530

    git experiment

--- accounts/src/main/java/com/udaipur/accounts/feign/clients/CardsFeignClient.java
+++ accounts/src/main/java/com/udaipur/accounts/feign/clients/CardsFeignClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.udaipur.accounts.model.Cards;
 import com.udaipur.accounts.model.Customer;
-
+// this line added to check git commands for experiment only
 @FeignClient("cards")
 public interface CardsFeignClient {
 	
